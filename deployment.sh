a2cc2ed8343a8711c3583246f6bdae1e7df05c94
EC2 Default User <ec2-user@ip-172-31-89-51.ec2.internal>
2019-10-17 07:46:07 +0000

    remove '-' in stack name.

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,7 @@
 contactFlowArn=arn:aws:connect:us-east-1:467005446488:instance/087c643b-fd04-4b8a-b2c1-b32c1e6de399/contact-flow/9163cea9-b9d6-4630-b315-920695e12e66
 sourcePhoneNumber=+18337325078
 
-export STACK_NAME=aws_callout
+export STACK_NAME=awscallout
 export AWS_DEFAULT_REGION=us-east-1
 export REGION=$AWS_DEFAULT_REGION
 
