0dc5353e609f93c9c2731690b226b822c292550e
EC2 Default User <ec2-user@ip-172-31-81-169.ec2.internal>
2019-12-11 09:47:31 +0000

    Updated with SAM Config and change SQS to FIFO queue.

--- deployment.sh
+++ deployment.sh
@@ -1,23 +1,18 @@
-instanceId=087c643b-fd04-4b8a-b2c1-b32c1e6de399
-contactFlowId=9163cea9-b9d6-4630-b315-920695e12e66
-sourcePhoneNumber=+18337325078
+echo "Deploy Stack"
 
-export STACK_NAME=awscallout
-export AWS_DEFAULT_REGION=us-east-1
-export REGION=$AWS_DEFAULT_REGION
+sam build
+sam deploy
 
-echo "Deploy $STACK_NAME stack at $REGION region"
+# sourcebucket=cyruswong-sam-repo
+# aws s3 mb s3://$sourcebucket --region $REGION 
+# rm package.yaml
+# sam package --template-file template.yaml --s3-bucket $sourcebucket --output-template-file package.yaml
 
-sourcebucket=cyruswong-sam-repo
-aws s3 mb s3://$sourcebucket --region $REGION 
-rm package.yaml
-sam package --template-file template.yaml --s3-bucket $sourcebucket --output-template-file package.yaml
-
-aws cloudformation deploy --stack-name $STACK_NAME --template-file package.yaml \
---region $REGION --capabilities CAPABILITY_IAM \
---parameter-overrides \
-    InstanceId=$instanceId \
-    ContactFlowId=$contactFlowId \
-    SourcePhoneNumber=$sourcePhoneNumber
+# aws cloudformation deploy --stack-name $STACK_NAME --template-file package.yaml \
+# --region $REGION --capabilities CAPABILITY_IAM \
+# --parameter-overrides \
+#     InstanceId=$instanceId \
+#     ContactFlowId=$contactFlowId \
+#     SourcePhoneNumber=$sourcePhoneNumber
 
 # sam publish --template package.yaml --region us-east-1
