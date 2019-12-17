8a8dd0b884c10f3ace71b9a27eee416cdaba2d74
EC2 Default User <ec2-user@ip-172-31-81-169.ec2.internal>
2019-12-17 00:42:33 +0000

    Add raw json result and presigned url to download excel report. Add back SNS topic ARN to stack output.

--- call_out/get_call_result.py
+++ call_out/get_call_result.py
@@ -4,6 +4,7 @@ import json
 import decimal
 import os
 from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
 
 import sys
 sys.path.append("/opt/")
@@ -30,13 +31,40 @@ def lambda_handler(event, context):
     df = pd.DataFrame(list(map(convert_answer, response[u'Items'])))
     filename = "/tmp/{}.xlsx".format(task_id)
     df.to_excel(filename)
-
     s3.upload_file(filename, os.environ['CallReportBucket'],
                    task_id + "_result.xlsx")
+
+    excelDownloadUrl = create_presigned_url(os.environ['CallReportBucket'],
+                                            task_id + "_result.xlsx")
+
+    filename = "/tmp/{}.json".format(task_id)
+    with open(filename, 'w') as json_file:
+        json_file.write(df.to_json(orient='records'))
+    s3.upload_file(filename, os.environ['CallReportBucket'],
+                   task_id + "_result.json")
+
     print(df)
 
     return {
         "TaskId": task_id,
         "Bucket": os.environ['CallReportBucket'],
-        "Key": task_id + "_result.xlsx"
+        "ExcelKey": task_id + "_result.xlsx",
+        "ExcelDownloadUrl": excelDownloadUrl,
+        "JsonKey": task_id + "_result.json"
     }
+
+
+def create_presigned_url(bucket_name, object_name, expiration=3600 * 24 * 30):
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={
+                                                        'Bucket': bucket_name,
+                                                        'Key': object_name
+                                                    },
+                                                    ExpiresIn=expiration)
+    except ClientError as e:
+        print(e)
+        return None
+
+    return response
