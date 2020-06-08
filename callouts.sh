ebf55d48fc0e214c87687f984f179ce73a7f1e09
EC2 Default User <ec2-user@ip-172-31-84-68.ec2.internal>
2020-06-08 06:16:49 +0000

    Change IAM S3 permission coding style.

--- callouts.sh
+++ callouts.sh
@@ -4,8 +4,8 @@ cd ExcelLexBot
 sudo ./get_layer_packages.sh
 source deployment.sh
 cd ..
-git clone https://github.com/wongcyrus/aws-callouts
-cd aws-callouts
+git clone https://github.com/wongcyrus/callouts
+cd callouts
 ./deploy_chatbot.sh
 ./setup.sh
 sudo ./get_layer_packages.sh
